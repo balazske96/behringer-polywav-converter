6e068105d2f395b795c65d1245ec7eeec69bbc0b
Venyige Balázs <venyigebalazs96@gmail.com>
2025-10-18 15:52:30 +0200

    refactor: directory checking refactored

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.ComponentModel.Design;
 using System.IO;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Runtime.CompilerServices;
 using NAudio.Wave;
 
@@ -18,6 +19,7 @@ namespace BehringerPolywavConverter
             // Check if the input arguments are given
             if (args.Length < 2)
             {
+                Console.Write("Not enough arguments provided. ");
                 Console.WriteLine("Usage: behringer-polywav-converter <input_folder> <output_folder>");
                 return;
             }
@@ -25,24 +27,39 @@ namespace BehringerPolywavConverter
             string inputDir = args[0];
             string outputDir = args[1];
 
-            // Check if the directory exists (or if the given path is a file)
+            ValidateDirectory(directory: inputDir, shouldCreate: false, shouldBeEmpty: false);
+            ValidateDirectory(directory: outputDir, shouldCreate: true, shouldBeEmpty: true);
+        }
+
+        public static void ValidateDirectory(string directory, bool shouldCreate, bool shouldBeEmpty)
+        {
+            string currentDir = Directory.GetCurrentDirectory();
+            string absolutePathToDirToCreate = $"{currentDir}/{directory}";
+
             try
             {
-                FileAttributes attr = File.GetAttributes(outputDir);
+                FileAttributes attr = File.GetAttributes(directory);
             }
             catch (FileNotFoundException)
             {
-                if (!Directory.Exists(outputDir))
+                // Check if the directory exists (or if the given path is a file)
+                if (!Directory.Exists(directory))
                 {
-                    Directory.CreateDirectory(outputDir);
-                    Console.WriteLine("Output directory does not exist. Creating...");
+                    Console.WriteLine($"Directory at \"{absolutePathToDirToCreate}\" does not exist.");
+
+                    // If it doesn't exist and it should be created then we create it here
+                    if (shouldCreate)
+                    {
+                        Console.WriteLine($"Creating new directory at \"{absolutePathToDirToCreate}\"...");
+                        Directory.CreateDirectory(directory);
+                    }
                 }
             }
 
             // If the directory exists, check if it's empty and ask the user to delete it first.
-            if (Directory.GetFileSystemEntries(outputDir).Length != 0)
+            if (shouldBeEmpty && Directory.GetFileSystemEntries(directory).Length != 0)
             {
-                Console.WriteLine("Output directory is not empty. Please clean the output folder first!");
+                Console.WriteLine($"Directory at \"{absolutePathToDirToCreate}\" directory is not empty. Please clean the output folder first!");
             }
         }
     }
