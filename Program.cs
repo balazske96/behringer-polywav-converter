5090572a65cf94af928fcbcf7049e895ca0bbf53
Venyige Balázs <venyigebalazs96@gmail.com>
2025-10-18 15:32:05 +0200

    feat: check if output directory is correct

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.ComponentModel.Design;
 using System.IO;
 using System.Linq;
-
+using System.Runtime.CompilerServices;
+using NAudio.Wave;
 
 namespace BehringerPolywavConverter
 {
@@ -9,7 +11,39 @@ namespace BehringerPolywavConverter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Introduction
+            Console.WriteLine("Polywav converter for Behringer Wing based records.");
+            Console.WriteLine("Usage: behringer-polywav-converter <input_folder> <output_folder>");
+
+            // Check if the input arguments are given
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Usage: behringer-polywav-converter <input_folder> <output_folder>");
+                return;
+            }
+
+            string inputDir = args[0];
+            string outputDir = args[1];
+
+            // Check if the directory exists (or if the given path is a file)
+            try
+            {
+                FileAttributes attr = File.GetAttributes(outputDir);
+            }
+            catch (FileNotFoundException)
+            {
+                if (!Directory.Exists(outputDir))
+                {
+                    Directory.CreateDirectory(outputDir);
+                    Console.WriteLine("Output directory does not exist. Creating...");
+                }
+            }
+
+            // If the directory exists, check if it's empty and ask the user to delete it first.
+            if (Directory.GetFileSystemEntries(outputDir).Length != 0)
+            {
+                Console.WriteLine("Output directory is not empty. Please clean the output folder first!");
+            }
         }
     }
 }
