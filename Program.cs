f762bacd982bd22ae705f70ce2c4678fc1a796a1
Venyige Balázs <venyigebalazs96@gmail.com>
2025-10-18 15:14:49 +0200

    chores: dotnet project initialized

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+
+namespace BehringerPolywavConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
